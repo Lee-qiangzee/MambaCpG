3a342e86513e4c1afe57d84c8f89bedcc1102c98
Ze Li <139248655+Lee-qiangzee@users.noreply.github.com>
2025-01-15 20:59:24 +0800

    Update impute.py

--- model/impute.py
+++ model/impute.py
@@ -23,25 +23,24 @@ parser.add_argument('y', type=str, metavar='y', help='NumPy file (.npz) containi
 parser.add_argument('pos', type=str, metavar='pos', help='NumPy file (.npz) containing positions of CpG sites.')
 parser.add_argument('output', type=str, metavar='output', help='NumPy file (.npz) containing output methylation matrix.')
 
+parser.add_argument('--model_checkpoint', type=str, default=None,
+                         help='.ckpt file containing the model to use. DOES NOT WORK WITH .pt STATE DICT FILES.')
+parser.add_argument('--segment_size', type=int, default=1024,
+                         help='Bin size in number of CpG sites (columns) that every batch will contain.')
+parser.add_argument('--DNA_window', type=int, default=1001,
+                         help='Haha')
+parser.add_argument('--n_workers', type=int, default=4,
+                      help='Number of worker threads to use in data loading. Increase if you experience a CPU bottleneck.')
+parser.add_argument('--device', type=str, choices=['CPU', 'GPU'], default='GPU',
+                         help='GPU or CPU. For inference, it is currently only possible to use 1 GPU.')
+
 optional_parse = parser.add_argument_group('General optional arguments.')
 
 optional_parse.add_argument('--keys', type=str, nargs='+', default=None,
                             help='Only impute chromosomes corresponding to these keys.')
-optional_parse.add_argument('--denoise', type=boolean, default=True,
+optional_parse.add_argument('--denoise', type=boolean, default=False,
                             help='If False, return the original methylation state for already-observed elements in the output. In other words: only unobserved elements will be imputed and observed sites will retain their original label always. If True, model predictions will be returned for all inputs, irregardless of whether they are observed.')
 
-cpgtf_parse = parser.add_argument_group('CpG Transformer-specific arguments.',
-                                        'These arguments are only relevant when imputing with CpG Transformer models.')
-cpgtf_parse.add_argument('--model_checkpoint', type=str, default=None,
-                         help='.ckpt file containing the model to use. DOES NOT WORK WITH .pt STATE DICT FILES.')
-cpgtf_parse.add_argument('--segment_size', type=int, default=1024,
-                         help='Bin size in number of CpG sites (columns) that every batch will contain.')
-cpgtf_parse.add_argument('--DNA_window', type=int, default=1001,
-                         help='Haha')
-cpgtf_parse.add_argument('--n_workers', type=int, default=4,
-                      help='Number of worker threads to use in data loading. Increase if you experience a CPU bottleneck.')
-cpgtf_parse.add_argument('--device', type=str, choices=['CPU', 'GPU'], default='GPU',
-                         help='GPU or CPU. For inference, it is currently only possible to use 1 GPU.')
 
 args = parser.parse_args()
 
@@ -56,7 +55,7 @@ model = MambaCpG.load_from_checkpoint(args.model_checkpoint)
 model.eval()
 model = model.to(dev)
 
-dm = MambaCpGImputingDataModule(X, y, pos, segment_size=args.segment_size, DNA_window=args.DNA_window,
+dm = MambaCpGImputingDataModule(X, y, pos, DNA_window=args.DNA_window, segment_size=args.segment_size, 
                                     keys=args.keys, n_workers=args.n_workers)
 print('Preprocessing data ...')
 dm.setup(None)
@@ -67,22 +66,25 @@ for key, loader in dm.datasets_per_chr.items():
     y_outputs_key = np.empty(y[key].shape)
 
     lenloader_key = len(loader)
+
+    a = y[key].shape[0] % args.segment_size
+    if a == 0:
+        a = args.segment_size
+
     for ix, batch in enumerate(loader):
         
-        batch = [batch[0].to(dev, torch.long), batch[1].to(dev, model.dtype), batch[3].to(dev, torch.long)]
+        batch = [batch[0].to(dev, torch.long), batch[1].to(dev, model.dtype), batch[2].to(dev, torch.long)]
         with torch.no_grad():
             output = model(*batch)
         
         output = torch.sigmoid(output[0]).to('cpu').numpy()
-            
-        if ix == 0:
-            y_outputs_key[:args.segment_size-RF2_TF] = output[:args.segment_size-RF2_TF]
-        elif ix+1 == lenloader_key:
-            loc = (args.segment_size-RF2_TF*2)*ix
-            y_outputs_key[loc+RF2_TF:] = output[RF2_TF:]
+                   
+        loc = (args.segment_size) * ix
+
+        if ix+1 == lenloader_key:
+            y_outputs_key[loc:] = output[:a]
         else:
-            loc = (args.segment_size-RF2_TF*2)*ix
-            y_outputs_key[loc+RF2_TF:loc+args.segment_size-RF2_TF] = output[RF2_TF:-RF2_TF]
+            y_outputs_key[loc:loc + args.segment_size] = output
 
     y_outputs[key] = y_outputs_key
 
@@ -90,6 +92,6 @@ if args.denoise == False:
     for key in y_outputs.keys():
         observed = np.where(y[key] != -1)
         y_outputs[key][observed] = y[key][observed]
-        
+
 np.savez_compressed(args.output, **y_outputs)
     
